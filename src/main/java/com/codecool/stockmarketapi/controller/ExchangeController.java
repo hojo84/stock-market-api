8f18bd626f41952aeb91ae949b1a60ebd205ef33
hojo84 <hojo84@gmail.com>
2022-05-19 10:26:43 +0200

    Create class ExchangeController

---
+++ src/main/java/com/codecool/stockmarketapi/controller/ExchangeController.java
@@ -0,0 +1,18 @@
+package com.codecool.stockmarketapi.controller;
+
+import com.codecool.stockmarketapi.service.ExchangeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exchanges")
+public class ExchangeController {
+
+    private final ExchangeService exchangeService;
+
+    @Autowired
+    public ExchangeController(ExchangeService exchangeService) {
+        this.exchangeService = exchangeService;
+    }
+}
