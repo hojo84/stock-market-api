f1e7e15ed072dda7be2695ebd11b5e3e30ef0fbb
hojo84 <hojo84@gmail.com>
2022-05-26 17:37:10 +0200

    Resolve autowire errors in service classes

--- src/main/java/com/codecool/stockmarketapi/service/IndexService.java
+++ src/main/java/com/codecool/stockmarketapi/service/IndexService.java
@@ -4,7 +4,6 @@ import com.codecool.stockmarketapi.dao.GenericCrudDAO;
 import com.codecool.stockmarketapi.dao.IndexDAO;
 import com.codecool.stockmarketapi.model.Index;
 import com.codecool.stockmarketapi.model.IndexComponent;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,14 +11,8 @@ import java.util.List;
 @Service
 public class IndexService {
 
-    private final GenericCrudDAO<Index> genericCrudDAO;
-    private final IndexDAO indexDAO;
-
-    @Autowired
-    public IndexService(GenericCrudDAO<Index> genericCrudDAO, IndexDAO indexDAO) {
-        this.genericCrudDAO = genericCrudDAO;
-        this.indexDAO = indexDAO;
-    }
+    private GenericCrudDAO<Index> genericCrudDAO;
+    private IndexDAO indexDAO;
 
     public List<Index> listAll() {
         return genericCrudDAO.listAll();
--- src/main/java/com/codecool/stockmarketapi/service/StockService.java
+++ src/main/java/com/codecool/stockmarketapi/service/StockService.java
@@ -4,7 +4,6 @@ import com.codecool.stockmarketapi.dao.GenericCrudDAO;
 import com.codecool.stockmarketapi.dao.StockDAO;
 import com.codecool.stockmarketapi.model.Index;
 import com.codecool.stockmarketapi.model.Stock;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,14 +11,8 @@ import java.util.List;
 @Service
 public class StockService {
 
-    private final GenericCrudDAO<Stock> genericCrudDAO;
-    private final StockDAO stockDAO;
-
-    @Autowired
-    public StockService(GenericCrudDAO<Stock> genericCrudDAO, StockDAO stockDAO) {
-        this.genericCrudDAO = genericCrudDAO;
-        this.stockDAO = stockDAO;
-    }
+    private GenericCrudDAO<Stock> genericCrudDAO;
+    private StockDAO stockDAO;
 
     public List<Stock> listAll() {
         return genericCrudDAO.listAll();
