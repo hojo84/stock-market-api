1b6b0606793f79921ed61efd31524ccd47e12543
hojo84 <hojo84@gmail.com>
2022-06-13 08:31:20 +0200

    Add ResponseStatus NO_CONTENT for delete mapping

--- src/main/java/com/codecool/stockmarketapi/controller/ExchangeController.java
+++ src/main/java/com/codecool/stockmarketapi/controller/ExchangeController.java
@@ -7,6 +7,7 @@ import com.codecool.stockmarketapi.service.ExchangeService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
@@ -60,6 +61,7 @@ public class ExchangeController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteById(@PathVariable("id") String id) {
         exchangeService.deleteById(id);
     }
