1ca8c0007a9d66dd0329ad86a5af8cf47a573e40
hojo84 <hojo84@gmail.com>
2022-06-15 12:11:39 +0200

    Implement test for ExchangeController::findById

--- src/test/java/com/codecool/stockmarketapi/ExchangeControllerUnitTests.java
+++ src/test/java/com/codecool/stockmarketapi/ExchangeControllerUnitTests.java
@@ -1,6 +1,7 @@
 package com.codecool.stockmarketapi;
 
 import com.codecool.stockmarketapi.controller.ExchangeController;
+import com.codecool.stockmarketapi.entity.Exchange;
 import com.codecool.stockmarketapi.service.ExchangeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -41,5 +43,21 @@ public class ExchangeControllerUnitTests {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.size()", equalTo(3)))
                 .andExpect(jsonPath("$[0]", equalTo("London Stock Exchange (LSE)")));
+    }
+
+    @Test
+    public void testFindExchangeById() throws Exception {
+        Exchange expected = new Exchange("XNAS", "Nasdaq", "New York", "USD", "www.nasdaq.com",
+                Collections.emptySet());
+
+        when(exchangeService.findById(expected.getId())).thenReturn(expected);
+
+        mockMvc.perform(get("/exchanges/{id}", expected.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo("Nasdaq")))
+                .andExpect(jsonPath("$.location", equalTo("New York")))
+                .andExpect(jsonPath("$.currency", equalTo("USD")))
+                .andExpect(jsonPath("$.website", equalTo("www.nasdaq.com")));
     }
 }
