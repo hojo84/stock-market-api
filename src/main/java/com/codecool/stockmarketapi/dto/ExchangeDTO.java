acdb49ed9bb537c504d4fb561eaed8a130b12605
hojo84 <hojo84@gmail.com>
2022-06-17 17:13:13 +0200

    Customize API doc for ExchangeDTO: add Swagger annotation @Schema

--- src/main/java/com/codecool/stockmarketapi/dto/ExchangeDTO.java
+++ src/main/java/com/codecool/stockmarketapi/dto/ExchangeDTO.java
@@ -1,5 +1,6 @@
 package com.codecool.stockmarketapi.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,14 +13,23 @@ import javax.validation.constraints.NotBlank;
 @AllArgsConstructor
 public class ExchangeDTO {
 
+    @Schema(description = "unique identification", example = "VSE")
     @NotBlank(message = "Exchange id must not be null or blank")
     private String id;
+
+    @Schema(description = "name of the exchange", example = "Vienna Stock Exchange")
     @NotBlank(message = "Exchange name must not be null or blank")
     private String name;
+
+    @Schema(description = "location of the exchange", example = "Vienna")
     @NotBlank(message = "Exchange location must not be null or blank")
     private String location;
+
+    @Schema(description = "currency of the exchange", example = "EUR")
     @NotBlank(message = "Exchange currency must not be null or blank")
     @Length(min = 3, max = 3, message = "Exchange currency must be a three-letter alphabetic code")
     private String currency;
+
+    @Schema(description = "official website of the exchange", example = "www.wienerborse.at")
     private String website;
 }
