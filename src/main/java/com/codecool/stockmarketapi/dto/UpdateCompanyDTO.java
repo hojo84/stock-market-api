c0cdcfa9c8ed1143e34e7225690397dafe744038
hojo84 <hojo84@gmail.com>
2022-06-17 18:01:51 +0200

    Customize API doc for UpdateCompanyDTO: add Swagger annotation @Schema

--- src/main/java/com/codecool/stockmarketapi/dto/UpdateCompanyDTO.java
+++ src/main/java/com/codecool/stockmarketapi/dto/UpdateCompanyDTO.java
@@ -1,6 +1,7 @@
 package com.codecool.stockmarketapi.dto;
 
 import com.codecool.stockmarketapi.entity.EquityType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,12 +14,19 @@ import javax.validation.constraints.NotNull;
 @AllArgsConstructor
 public class UpdateCompanyDTO {
 
+    @Schema(description = "unique identification", example = "VZ")
     @NotBlank(message = "Company id must not be null or blank")
     private String id;
+
+    @Schema(description = "name of the company", example = "Verizon Communications Inc.")
     @NotBlank(message = "Company name must not be null or blank")
     private String name;
+
+    @Schema(description = "sector of the company", example = "Communications")
     @NotBlank(message = "Sector must not be null or blank")
     private String sector;
+
+    @Schema(description = "types of equity are different forms of shares or ownership available in a company", example = "COMMON_STOCK")
     @NotNull(message = "EquityType must not be null")
     private EquityType equityType;
 }
