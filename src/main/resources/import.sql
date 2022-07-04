842510751ebe65f7636fd85b78dbd8c02d0ac3a9
hojo84 <hojo84@gmail.com>
2022-07-04 15:11:41 +0200

    Implement endpoint for adding exchange to country

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,12 +2,14 @@ insert into countries (id, name) values(1, 'United States');
 insert into countries (id, name) values(2, 'Hungary');
 insert into countries (id, name) values(3, 'China');
 insert into countries (id, name) values(4, 'Great Britain');
+insert into countries (id, name) values(5, 'France');
 
 insert into exchanges (id, currency, location, name, website, country_id) values('NYSE', 'USD', 'New York', 'New York Stock Exchange', 'www.nyse.com', 1);
 insert into exchanges (id, currency, location, name, website, country_id) values('XNAS', 'USD', 'New York', 'Nasdaq Stock Market', 'www.nasdaq.com', 1);
 insert into exchanges (id, currency, location, name, website, country_id) values('SSE', 'CNY', 'Shanghai', 'Shanghai Stock Exchange', 'english.sse.com.cn', 3);
 insert into exchanges (id, currency, location, name, website, country_id) values('BUD', 'HUF', 'Budapest', 'Budapest Stock Exchange Index', 'bet.hu', 2);
 insert into exchanges (id, currency, location, name, website, country_id) values('LSE', 'GBP', 'London', 'London Stock Exchange', 'www.londonstockexchange.com', 4);
+insert into exchanges (id, currency, location, name, website, country_id) values('XPAR', 'EUR', 'Paris', 'Euronext Paris', 'https://www.euronext.com/en/markets/paris', null);
 
 insert into companies (id, equity_type, name, sector) values ('AAPL', 'COMMON_STOCK', 'Apple', 'Information Technology');
 insert into companies (id, equity_type, name, sector) values ('OTP', 'COMMON_STOCK', 'OTP Nyrt', 'Financials');
