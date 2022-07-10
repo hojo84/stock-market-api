ebec058f97f1d0ec39d43475a2c172850b491119
hojo84 <hojo84@gmail.com>
2022-07-10 10:37:19 +0200

    Customize API documentation for CreateListingDTO: add field description and examples using @Schema annotation

--- src/main/java/com/codecool/stockmarketapi/dto/CreateListingDTO.java
+++ src/main/java/com/codecool/stockmarketapi/dto/CreateListingDTO.java
@@ -1,6 +1,7 @@
 package com.codecool.stockmarketapi.dto;
 
 import com.codecool.stockmarketapi.entity.EquityType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,13 +13,18 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class CreateListingDTO {
 
+    @Schema(description = "unique identification for listing", example = "AAPLB")
     private String id;
 
+    @Schema(description = "unique identification for exchange", example = "NYSE")
     private String exchangeId;
 
+    @Schema(description = "unique identification for company", example = "AAPL")
     private String companyId;
 
+    @Schema(description = "equity types are different forms of shares or ownership available in a company", example = "COMMON_STOCK")
     private EquityType equityType;
 
+    @Schema(description = "date of Initial Public Offering", example = "2022-03-05")
     private LocalDate ipo;
 }
